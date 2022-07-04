0bf0babda48f45555ea5d2ef6fed208ba27bdcce
Kaden <Kaden@ASUS-Laptop>
2022-07-04 07:37:56 -0600

    Age Servlet

--- src/java/servlets/ArithmeticCalculatorServlet.java
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -1,28 +1,30 @@
+
 package servlets;
 
-import com.sun.org.apache.bcel.internal.generic.RETURN;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AgeCalculatorServlet extends HttpServlet {
+
+@WebServlet(name = "ArithmeticCalculatorServlet", urlPatterns = {"/arithmetic"})
+public class ArithmeticCalculatorServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
         // Load JSP
-        getServletContext().getRequestDispatcher("/WEB-INF/AgeCalculator.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp").forward(request, response);
         return;
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
     }
+
 
 }
---
+++ web/WEB-INF/arithmeticcalculator.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : arithmeticcalculator
+    Created on : 4-Jul-2022, 7:34:11 AM
+    Author     : Kaden
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Arithmetic Calculator</title>
+    </head>
+    <body>
+        <h1>Arithmetic Calculator</h1>
+    </body>
+</html>
